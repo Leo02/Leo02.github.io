5ac82566786e8027b1b636f74341134af0d91e2b
Leo02 <dee2138708@maricopa.edu>
2017-07-01 20:53:17 +0800

    delete

--- i/config.js
+++
@@ -1,7 +0,0 @@
-var _config = {
-    blog_name       : '资源聚合',         // 博客名称
-    owner           : 'Leo02',           // github 用户名
-    repo            : 'Leo02.github.io',// github 中对应项目名
-    access_token : 'ab24e86122ffc43f75d'+'25e1ab263c8af082f8952',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
-    per_page        : '15'                    // 默认一页显示几篇文章
-}
